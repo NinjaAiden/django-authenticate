81f3a880c4dfc688e9312dacfba0cc4f219101b7
Aiden Harvey <aidenharvey102@gmail.com>
2019-06-01 10:04:46 +0000

    added basic login template

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse
 from django.contrib import auth, messages
 
+from accounts.forms import UserLoginForm
+
 def index(request):
     """ Return index html file """
     return render(request, "index.html")
@@ -10,3 +12,8 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out!")
     return redirect(reverse('index'))
+
+def login(request):
+    """ Return a login page """
+    login_form = UserLoginForm()
+    return render(request, "login.html", {"login_form": login_form})
--- django_auth/urls.py
+++ django_auth/urls.py
@@ -17,10 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 # import functions from app for rendering urls
-from accounts.views import index, logout
+from accounts.views import index, logout, login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
-    url(r'^accounts/logout/$', logout, name="logout")
+    url(r'^accounts/logout/$', logout, name="logout"),
+    url(r'^accounts/login/$', login, name="login")
 ]
