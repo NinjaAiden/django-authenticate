d4cd100dcc68b80ca6acff3b8a788a314e7d9320
Aiden Harvey <aidenharvey102@gmail.com>
2019-06-01 09:21:25 +0000

    added basic routing

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def index(request):
+    """ Return index html file """
+    return render(request, "index.html")
